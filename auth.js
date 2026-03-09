bdeb1d6727a9fd05a15911a33acdcace645cfa4f
Cethin <cethinthegreat@gmail.com>
2026-03-09 16:48:43 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -55,3 +55,63 @@ alert("Invalid username or password");
 }
 
 });
+
+
+
+
+// LOGIN
+
+document.getElementById("loginSubmit").addEventListener("click", function(){
+
+const username = document.getElementById("loginUsername").value;
+const password = document.getElementById("loginPassword").value;
+
+const savedUser = JSON.parse(localStorage.getItem("user"));
+
+const ADMIN_USERNAME = "Cethin_admin<3";
+const ADMIN_PASSWORD = "IamTheCutestAdmin<3";
+
+
+/* ADMIN LOGIN */
+
+if(username === ADMIN_USERNAME && password === ADMIN_PASSWORD){
+
+localStorage.setItem("loggedInUser", JSON.stringify({
+username: ADMIN_USERNAME,
+password: ADMIN_PASSWORD
+}));
+
+alert("Admin login successful!");
+
+closeModal();
+showAdminButton();
+
+return;
+
+}
+
+
+  
+/* NORMAL USER LOGIN */
+
+if(!savedUser){
+
+alert("No account found. Please sign up.");
+return;
+
+}
+
+if(username === savedUser.username && password === savedUser.password){
+
+localStorage.setItem("loggedInUser", JSON.stringify(savedUser));
+
+alert("Login successful!");
+closeModal();
+
+}else{
+
+alert("Invalid username or password");
+
+}
+
+});
